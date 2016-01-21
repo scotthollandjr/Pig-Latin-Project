e5cf95da3e61430336994a36453ebda3ce16818b
Scott Holland <scotthollandjr@gmail.com>
2016-01-21 13:29:17 -0800

    Add vowel first functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-scripts.js
+var pigged;
+var string;
+
+function vowel1(string) {
+  if (string.charAt(0) === ( "a" || "e" || "i" || "o" || "u" )) {
+    pigged = string.concat( "ay" );
+    return pigged;
+  } else {
+    consonFirst(string);
+  }
+}
+
+function consonant1(string) {
+
+}
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,11 @@
-describe('#', function() {
-  it("wut it does", function() {
-    expect(#()).to.equal()
+describe('vowel1', function() {
+  it("words beginning with vowel, append ay", function() {
+    expect(vowel1("apple")).to.equal("appleay")
   });
 });
+
+// words beginning with vowel, append ay
+// words beginning with 1 consonant, move to the end of the word and append ay
+// words beginning with 2 consonants, move to the end of the word and append ay
+// words beginning with qu, move to end of the word and append ay
+// treat y as consonant
