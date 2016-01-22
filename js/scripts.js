c617f8c6e543bc0690d09139d0f68b36bd62e42a
Scott Holland <scotthollandjr@gmail.com>
2016-01-21 17:13:02 -0800

    Add user interface, with tweaks to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,43 @@ var pigged;
 var original;
 
 function vowel1(original) {
-  if (original.charAt(0) === ( "a" || "e" || "i" || "o" || "u" )) {
+  var firstChar = original.charAt(0);
+  if ((firstChar === "a" ) || (firstChar === "e" ) || (firstChar === "i" ) || (firstChar === "o" ) || (firstChar === "u" )) {
     pigged = original.concat( "ay" );
+    alert("ran vowel1");
     return pigged;
-  }
-}
-
-function consonant2(original) {
-  if (original.charAt(1) !== ( "a" || "e" || "i" || "o" || "u" )) {
-    pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
-    return pigged;
+  } else {
+    consonant1(original);
   }
 }
 
 function consonant1(original) {
-  if (original.charAt(0) != ( "q" )) {
+  var secondChar = original.charAt(1);
+  if (((secondChar === "a" ) || (secondChar === "e" ) || (secondChar === "i" ) || (secondChar === "o" ) || (secondChar === "u" )) && (original.charAt(0) != "q" )){
     pigged = original.substr(1) + original.charAt(0) + "ay";
+    alert("ran consonant1");
+    return pigged;
+  } else if (original.charAt(0) === "q" ){
+    pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
+    alert("ran q function");
     return pigged;
   } else {
-    pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
-    return pigged;
+    consonant2(original);
   }
 }
+
+function consonant2(original) {
+    pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
+    alert("ran consonant2");
+    return pigged;
+}
+
+$(document).ready(function() {
+  $("form#piggy").submit(function(event) {
+    var entry = $("input#word").val();
+
+    vowel1(entry);
+    alert(pigged);
+    event.preventDefault();
+  });
+});
