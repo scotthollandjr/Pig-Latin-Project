a847e62503584b3f52464737f063a0f8e49800e7
Scott Holland <scotthollandjr@gmail.com>
2016-01-21 17:14:23 -0800

    Remove debugging alerts

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function vowel1(original) {
   var firstChar = original.charAt(0);
   if ((firstChar === "a" ) || (firstChar === "e" ) || (firstChar === "i" ) || (firstChar === "o" ) || (firstChar === "u" )) {
     pigged = original.concat( "ay" );
-    alert("ran vowel1");
     return pigged;
   } else {
     consonant1(original);
@@ -16,11 +15,9 @@ function consonant1(original) {
   var secondChar = original.charAt(1);
   if (((secondChar === "a" ) || (secondChar === "e" ) || (secondChar === "i" ) || (secondChar === "o" ) || (secondChar === "u" )) && (original.charAt(0) != "q" )){
     pigged = original.substr(1) + original.charAt(0) + "ay";
-    alert("ran consonant1");
     return pigged;
   } else if (original.charAt(0) === "q" ){
     pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
-    alert("ran q function");
     return pigged;
   } else {
     consonant2(original);
@@ -29,7 +26,6 @@ function consonant1(original) {
 
 function consonant2(original) {
     pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
-    alert("ran consonant2");
     return pigged;
 }
 
