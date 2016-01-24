9295acf47c937f88f0e2ff0abbc64a8fbbbae5ca
Scott Holland <scotthollandjr@gmail.com>
2016-01-24 10:35:12 -0800

    Add multiple word functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,48 @@
 var pigged;
+var entry;
 var original;
+var pigArray = [];
 
 function vowel1(original) {
   var firstChar = original.charAt(0);
   if ((firstChar === "a" ) || (firstChar === "e" ) || (firstChar === "i" ) || (firstChar === "o" ) || (firstChar === "u" )) {
     pigged = original.concat( "ay" );
-    return pigged;
+    pigArray.push(pigged + " ");
+
   } else {
     consonant1(original);
   }
-}
+};
 
 function consonant1(original) {
   var secondChar = original.charAt(1);
   if (((secondChar === "a" ) || (secondChar === "e" ) || (secondChar === "i" ) || (secondChar === "o" ) || (secondChar === "u" )) && (original.charAt(0) != "q" )){
     pigged = original.substr(1) + original.charAt(0) + "ay";
-    return pigged;
+    pigArray.push(pigged + " ");
   } else if (original.charAt(0) === "q" ){
     pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
-    return pigged;
+    pigArray.push(pigged + " ");
   } else if (original.charAt(1) === "q" ){
     pigged = original.substr(3) + original.charAt(0) + original.charAt(1) + original.charAt(2) + "ay"
   } else {
     consonant2(original);
   }
-}
+};
 
 function consonant2(original) {
     pigged = original.substr(2) + original.charAt(0) + original.charAt(1) + "ay";
-    alert("run consonant2");
-    return pigged;
-}
+    pigArray.push(pigged + " ");
+};
+
 
 $(document).ready(function() {
   $("form#piggy").submit(function(event) {
-    var entry = $("input#word").val();
+    entry = $("input#word").val();
+    var entryArr = entry.split(" ");
 
-    vowel1(entry);
-    alert(pigged);
+    entryArr.forEach(vowel1);
+
+    $("h2#result").append(pigArray);
     event.preventDefault();
   });
 });
